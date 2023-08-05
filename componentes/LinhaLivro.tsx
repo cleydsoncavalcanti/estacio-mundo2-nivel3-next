d2786e2522fa5f358da303d6ec93eb19d6a3188b
Cavalcanti <cleydsoncavalcanti@gmail.com>
2023-08-05 10:20:32 -0300

    🐛 colunas lista de livros

--- componentes/LinhaLivro.tsx
+++ componentes/LinhaLivro.tsx
@@ -15,9 +15,17 @@ const LinhaLivro = ({
 
   return (
     <tr>
-      <td>{livro.título}</td>
-      <td>{livro.autores.join(", ")}</td>
+      <td>
+        <div className="d-flex flex-column">
+          {livro.título}
+          <button onClick={excluir} className="btn btn-danger">
+            Excluir
+          </button>
+        </div>
+      </td>
+      <td>{livro.resumo}</td>
       <td>{controleEditora.getNomeEditora(livro.codEditora)}</td>
+      <td>{livro.autores.join(", ")}</td>
       <td>
         <button onClick={excluir}>Excluir</button>
       </td>
